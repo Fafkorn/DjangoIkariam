f57fb71318de1f2dd5ee171581a49d5a192e5802
PawelWojcik01 <39125441+PawelWojcik01@users.noreply.github.com>
2021-07-23 18:41:22 +0200

    naprawa media files

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,11 +119,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/helper/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 MESSAGE_TAGS = {
