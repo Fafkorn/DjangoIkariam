84ad775484b0c0916bab744b0d900ab0f4b70cb1
PawelWojcik01 <39125441+PawelWojcik01@users.noreply.github.com>
2021-07-24 14:02:52 +0200

    18

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 's&*1mgd2qn*_!dj)lz1q6e=q*h+jdlidomvjln@@pn8(6#_fwq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ['https://django-ika.herokuapp.com']
 
 
 # Application definition
